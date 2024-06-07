bbb5af696bcd096fde4841d1864542d61fe095eb
adanorabi <adanorabi12@gmail.com>
2024-06-07 20:54:34 +0300

    editing the app

--- Project/Frontend/src/App.js
+++ Project/Frontend/src/App.js
@@ -7,8 +7,6 @@ import {
 } from 'react-router-dom';
 
 import Users from './user/pages/Users';
-import NewPlace from './places/pages/NewPlace';
-import UserPlaces from './places/pages/UserPlaces';
 import MainNavigation from './shared/components/Navigation/MainNavigation';
 
 const App = () => {
@@ -20,12 +18,7 @@ const App = () => {
           <Route path="/" exact>
             <Users />
           </Route>
-          <Route path="/:userId/places" exact>
-            <UserPlaces />
-          </Route>
-          <Route path="/places/new" exact>
-            <NewPlace />
-          </Route>
+        
           <Redirect to="/" />
         </Switch>
       </main>
